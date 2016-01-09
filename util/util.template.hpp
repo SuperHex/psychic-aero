98404c83bff5b3fcae842804f212365757038786
Soei Wong <wang_ruihan@hotmail.com>
2016-01-09 23:38:14 +0800

    add fmap and foldr

--- util/util.template.hpp
+++ util/util.template.hpp
@@ -23,6 +23,38 @@ namespace util
                                   right[2] * left};
         return opt;
     }
+
+    // fmap :: (a -> b) -> f a -> f b
+    template< typename Func,
+              typename U,
+              template< typename ... > class T >
+    T<U> fmap(const Func& function, const T<U>& t)
+    {
+        T<U> res;
+        res.push_back(function(t.begin()));
+        if (t.next() != nullptr) // nullptr is not good
+        {
+            fmap(function, t.move_next(1));
+        }
+        return res;
+    }
+
+    // foldr :: (a -> b -> b) -> b -> t a -> b
+    template< typename Func,
+              typename Ta,
+              typename Tb,
+              template< typename ... > class T >
+    Tb foldr(Func function, Tb b, T<Ta> t)
+    {
+        if (t.begin() == nullptr) // maybe monoid instead ?
+        {
+            return b;
+        }
+        else
+        {
+            return function(t.begin(), foldr(function, b, t.move_next(1)));
+        }
+    }
 }
 
 #endif
