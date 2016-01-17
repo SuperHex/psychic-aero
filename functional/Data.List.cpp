f356837130c583a251155a944cba1746116f0c3d
Soei Wong <wang_ruihan@hotmail.com>
2016-01-17 23:53:56 +0800

    unmatured List

--- functional/Data.List.cpp
+++ functional/Data.List.cpp
@@ -1,39 +1,59 @@
 #ifndef __Data_List__
 #define __Data_List__
 
+#include <typeinfo>
+
 namespace Data
 {
-    template< typename a >
-    struct basic_list
+    namespace List
     {
-        a value;
-        basic_list<a>* next;
-        basic_list();
-        virtual ~basic_list(){ delete next; };
-    };
 
-    template< typename a >
-    struct Nil : public basic_list<a> { };
-
-    template< typename a >
-    struct List
-    {
-        template< typename Head, typename ... Tail >
-        basic_list<a>* make_list(const Head& h, const Tail& ... t)
+        template< typename a >
+        struct basic_list
         {
-            auto node = new basic_list<Head>();
-            node->value = h;
-            node->next = make_list(t ...);
-        }
+        public:
+            a value;
+            basic_list<a>* next;
+            basic_list();
+            virtual ~basic_list(){ delete next; };
+        };
 
-        template< typename Last >
-        basic_list<Last>* make_list(const Last& l)
+        template< typename a >
+        struct Nil : public basic_list<a> { };
+
+        template< typename a >
+        struct List
         {
-            auto node = new basic_list<Last>();
-            node->value = l;
-            node->next = new Nil<Last>();
+            template< typename Head, typename ... Tail >
+            basic_list<a>* make_list(const Head& h, const Tail& ... t)
+            {
+                auto node = new basic_list<Head>();
+                node->value = h;
+                node->next = make_list(t ...);
+            }
+
+            template< typename Last >
+            basic_list<Last>* make_list(const Last& l)
+            {
+                auto node = new basic_list<Last>();
+                node->value = l;
+                node->next = new Nil<Last>();
+            }
+        };
+
+        template< typename a >
+        a head(const basic_list<a>& list)
+        {
+            if (typeid(list) != typeid(Nil<a>))
+            {
+                return list.value;
+            }
+            else
+            {
+                throw new Nil<a>();
+            }
         }
-    };
+    }
 }
 
 #endif
--- functional/functional.hpp
+++ functional/functional.hpp
@@ -1,129 +1,146 @@
 #ifndef __Functional__
 #define __Functional__
 
-namespace until
+#include "Data.List.cpp"
+
+template< typename T >
+struct Monoid
 {
-    template< typename T >
-    struct Monoid
-    {
-    public:
-        Monoid();
-        virtual ~Monoid();
-        static T mempty();
-        static T mappend(const T&, const T&);
-    };
+public:
+    Monoid();
+    virtual ~Monoid();
+    static T mempty();
+    static T mappend(const T&, const T&);
+};
 
-    template< typename T >
-    struct Maybe
-    {
-    public:
-        T value;
-        bool isNothing = true;
-    };
+template< typename T >
+struct Maybe
+{
+public:
+    T value;
+    bool isNothing = true;
+};
 
+template< typename a >
+struct Monoid<Maybe<a>>
+{
+    static Maybe<a> mempty()
+    {
+        Maybe<a> r;
+        return r;
+    }
+
+    static Maybe<a> mappend(const Maybe<a>& a1, const Maybe<a>& a2)
+    {
+        if (a1.isNothing || a2.isNothing)
+        {
+            Maybe<a> no;
+            return no;
+        }
+        else
+        {
+            return mappend(a1.value, a2.value);
+        }
+    }
+};
+
+template< template< typename ... > class f >
+struct Functor
+{
+    template< typename a, typename b >
+    static f<b> fmap(b(*)(const a&), const f<a>&);
+};
+
+template< template< typename ... > class f >
+struct Applicative
+{
     template< typename a >
-    struct Monoid<Maybe<a>>
+    static f<a> pure(const a&);
+
+    template< typename a, typename b >
+    static f<b> ap(const f<b(*)(const a&)>, const f<a>&);
+};
+
+template< template< typename ... > class m >
+struct Monad
+{
+    template< typename a >
+    static m<a> mreturn(const a&);
+
+    template< typename a, typename b >
+    static m<b> mbind(const m<a>&, const m<b>(*)(const a&));
+};
+
+template< >
+struct Functor<Maybe>
+{
+    template< typename a, typename b >
+    static Maybe<b> fmap(b(*function)(const a&), Maybe<a> ma)
     {
-        static Maybe<a> mempty()
+        Maybe<b> r;
+        if (!ma.isNothing)
         {
-            Maybe<a> r;
-            return r;
+            r.value = function(ma.value);
         }
+        return r;
+    }
+};
 
-        static Maybe<a> mappend(const Maybe<a>& a1, const Maybe<a>& a2)
-        {
-            if (a1.isNothing || a2.isNothing)
-            {
-                Maybe<a> no;
-                return no;
-            }
-            else
-            {
-                return mappend(a1.value, a2.value);
-            }
-        }
-    };
-
-    template< template< typename ... > class f >
-    struct Functor
+template< >
+struct Applicative<Maybe>
+{
+    template< typename a >
+    static Maybe<a> pure(const a& val)
     {
-        template< typename a, typename b >
-        static f<b> fmap(b(*)(const a&), const f<a>&);
-    };
+        return mreturn(val);
+    }
 
-    template< template< typename ... > class f >
-    struct Applicative
+    template< typename a, typename b >
+    static Maybe<b> ap(const Maybe<b(*)(const a&)> function, const Maybe<a>& x)
     {
-        template< typename a >
-        static f<a> pure(const a&);
+        return fmap(function.value, x);
+    }
+};
 
-        template< typename a, typename b >
-        static f<b> ap(const f<b(*)(const a&)>, const f<a>&);
-    };
-
-    template< template< typename ... > class m >
-    struct Monad
+template< >
+struct Monad<Maybe>
+{
+    template< typename a >
+    static Maybe<a> mreturn(const a& v)
     {
-        template< typename a >
-        static m<a> mreturn(const a&);
+        Maybe<a> r;
+        r.isNothing = false;
+        r.value = v;
+        return r;
+    }
 
-        template< typename a, typename b >
-        static m<b> mbind(const m<a>&, const m<b>(*)(const a&));
-    };
-
-    template< >
-    struct Functor<Maybe>
+    template< typename a, typename b >
+    static Maybe<b> mbind(const Maybe<a>& ma, const Maybe<b>(*function)(const a&))
     {
-        template< typename a, typename b >
-        static Maybe<b> fmap(b(*function)(const a&), Maybe<a> ma)
+        if (ma.isNothing) return ma;
+        else
         {
-            Maybe<b> r;
-            if (!ma.isNothing)
-            {
-                r.value = function(ma.value);
-            }
-            return r;
+            return function(ma.value);
         }
-    };
+    }
+};
 
-    template< >
-    struct Applicative<Maybe>
+template< >
+struct Functor<Data::List::basic_list>
+{
+    template< typename a, typename b >
+    static Data::List::basic_list<b> fmap(b(*function)(const a&),
+                                          const Data::List::basic_list<a>& list)
     {
-        template< typename a >
-        static Maybe<a> pure(const a& val)
+        if (typeid(list) != typeid(Data::List::Nil<a>))
         {
-            return mreturn(val);
+            return Data::List::concat(function(list.value), fmap(list->next));
         }
-
-        template< typename a, typename b >
-        static Maybe<b> ap(const Maybe<b(*)(const a&)> function, const Maybe<a>& x)
+        else
         {
-            return fmap(function.value, x);
+            return list;
         }
-    };
-
-    template< >
-    struct Monad<Maybe>
-    {
-        template< typename a >
-        static Maybe<a> mreturn(const a& v)
-        {
-            Maybe<a> r;
-            r.isNothing = false;
-            r.value = v;
-            return r;
-        }
-
-        template< typename a, typename b >
-        static Maybe<b> mbind(const Maybe<a>& ma, const Maybe<b>(*function)(const a&))
-        {
-            if (ma.isNothing) return ma;
-            else
-            {
-                return function(ma.value);
-            }
-        }
-    };
-}
+    }
+};
 
 #endif
